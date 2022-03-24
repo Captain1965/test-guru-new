42bd2ee9ab5de843e68b4905156429784b48d57d
Captain1965 <alex1965-03@mail.ru>
2022-03-24 17:10:14 +0300

    changed test.rb

--- db/migrate/20220321142755_create_tests.rb
+++ db/migrate/20220321142755_create_tests.rb
@@ -3,7 +3,7 @@ class CreateTests < ActiveRecord::Migration[6.0]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, default: 1, null: false
-      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
       t.references :author_id, null: false, foreign_key: {to_table: :users}
       t.timestamps
     end
