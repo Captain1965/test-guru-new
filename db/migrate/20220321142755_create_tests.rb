a46e381c9a5c234410e4eabb21200c6d255747b9
Captain1965 <alex1965-03@mail.ru>
2022-03-24 12:26:56 +0300

    the sequence of loading migrations has been changed

--- db/migrate/20220321142755_create_tests.rb
+++ db/migrate/20220321142755_create_tests.rb
@@ -2,8 +2,8 @@ class CreateTests < ActiveRecord::Migration[6.0]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level, from: nil, to: 1, null: false
-      t.string :author, null: false
+      t.integer :level, default: 1, null: false
+      t.references :user, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -41,12 +41,13 @@ ActiveRecord::Schema.define(version: 2022_03_21_144726) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", null: false
-    t.string "author", null: false
+    t.integer "level", default: 1, null: false
+    t.bigint "user_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,4 +60,5 @@ ActiveRecord::Schema.define(version: 2022_03_21_144726) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
