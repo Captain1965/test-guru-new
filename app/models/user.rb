a1c08390a718503a31f1685e3bf33c0e0affbb67
Captain1965 <alex1965-03@mail.ru>
2022-03-29 19:39:38 +0300

    changed name method test_level_by_user(level) only it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  def test_level(level)
+  def test_level_by_user(level)
     Test
     .joins("JOIN tests ON author_id = users.id")
     .where(level: level, user: { user_id: id })
