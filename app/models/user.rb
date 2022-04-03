3b269445f7360eea367113fe207b59f9a38dedb4
Captain1965 <alex1965-03@mail.ru>
2022-04-03 23:34:07 +0300

    changed model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :tests, foreign_key: "author_id"
   has_and_belongs_to_many :tests
+
   def tests_by_level(level)
     Test
       .joins('JOIN tests_users ON tests.id = tests_users.test_id')
