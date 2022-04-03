9059330ea4bf453cee08b5ca7fc9de755036d281
Captain1965 <alex1965-03@mail.ru>
2022-04-03 23:30:44 +0300

    add association in model answer, category,question, test, user

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,12 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :user
+  has_many :questions
+  has_and_belongs_to_many :users
+
   def self.names_by_category(name)
     joins('JOIN categories ON categories.id = tests.category_id')
       .where(categories: { title: name }).pluck(:title)
   end
+
 end
