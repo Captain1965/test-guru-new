6c3ab1afa66e3877e81e0646ecb01afc8020a2f8
Captain1965 <alex1965-03@mail.ru>
2022-04-06 00:47:47 +0300

    refactoring models question, test, user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ApplicationRecord
-  has_many :tests, dependent: :destroy, foreign_key: "author_id"
+  has_many :created_tests, class_name:"tests", dependent: :destroy, foreign_key: "author_id"
   has_many :tests, through: :user_tests
 
-  belongs_to :tests_user
-
-  def tests_by_level(level)
-    Test
-      .joins('JOIN tests_users ON tests.id = tests_users.test_id')
-      .where(level: level, tests_users: { user_id: id })
+  def test_by_level(level)
+    tests.where(level: level)
   end
 end
