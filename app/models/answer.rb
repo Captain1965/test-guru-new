b38119f050e7685b5d6b369d2dfb52ac1fae3a67
Captain1965 <alex1965-03@mail.ru>
2022-04-12 11:31:59 +0300

    refactoring model Answer, method validate run only create new record

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :answers_limit
+  validate :answers_limit, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
-  def answer_limit
+  def answers_limit
     errors.add(:question, 'the number of responses is limited to 4') if question.answers.count >= 4
   end
 end
