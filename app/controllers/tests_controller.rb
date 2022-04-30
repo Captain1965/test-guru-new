3e9062655947b45687233dae43db62068bb19ecb
Captain1965 <alex1965-03@mail.ru>
2022-04-30 17:01:57 +0300

    lesson 7 variant1

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,17 +1,57 @@
 class TestsController < ApplicationController
-  before_action :test_find, only: %i[index show]
+  before_action :tests_find, only: %i[index new show edit]
+  before_action :test_find, only: %i[show edit destroy update]
 
   def index
-    render inline: '<%= @tests%>'
   end
 
   def show
-    render inline: '<%= @tests.find(params[:id])%>'
+    @questions = test_find.questions
+  end
+
+  def edit
+    #@test = test_find
+  end
+
+  def new
+
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+   end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test = test_find
+    @test.destroy
+    redirect_to tests_path
   end
 
   private
 
-  def test_find
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
+  def tests_find
     @tests = Test.all
+  end
+
+  def test_find
+    @test = Test.find(params[:id])
   end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+
+  def question_header(question)
+    if question.persisted?
+      "Edit question : #{question.body}"
+    else
+      "Create question"
+    end
+  end
 end
