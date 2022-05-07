bc99d794953e7336f22a34fb749a117667e1846c
Captain1965 <alex1965-03@mail.ru>
2022-05-07 19:53:38 +0300

    create model test_passages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :created_tests, class_name:"tests", dependent: :destroy, foreign_key: "author_id"
-  has_many :tests_users, dependent: :destroy
-  has_many :tests, through: :tests_users
+  has_many :test_passages
+  has_many :tests, through: :test_passages
 
   validates :email, presence: true
 
