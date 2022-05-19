99742849c7c89e921b264422521db7952e524132
Captain1965 <alex1965-03@mail.ru>
2022-05-19 06:44:48 +0300

    add validate email and login true

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :login, presence: true
+  validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
+
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
   end
