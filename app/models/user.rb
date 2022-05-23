fcb89b36aa59598948bc473b4c7092e7c4e37656
Captain1965 <alex1965-03@mail.ru>
2022-05-24 01:24:17 +0300

    changed model user and controller test_passages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,14 @@ require 'digest/sha1'
 class User < ApplicationRecord
 
   has_many :created_tests, class_name:"tests", dependent: :destroy, foreign_key: "author_id"
-  has_many :test_passages
+  has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
-
-  has_secure_password
 
   validates :login, presence: true
   validates :email, uniqueness: true,
                     format: { with: URI::MailTo::EMAIL_REGEXP, message: 'format email : name@post.com' }
 
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
