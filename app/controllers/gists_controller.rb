d28cb8f1d086da57d26a08784b3828c6ebaa09a1
Captain1965 <alex1965-03@mail.ru>
2022-11-21 02:32:43 +0300

    add .env.examle

---
+++ app/controllers/gists_controller.rb
@@ -0,0 +1,24 @@
+class GistsController < ApplicationController
+
+  before_action :set_test_passage
+
+  def create
+    result = GistQuestionService.new(@test_passage.current_question).call
+    if result.html_url != nil
+      current_user.gists.create!(question: @test_passage.current_question,
+                                 url: result.html_url)
+      flash[:notice] = "#{t('.success')} #{view_context.link_to(t('.view'),
+                                                                result.html_url,
+                                                                target: "_blank")}"
+    else
+      flash[:alert] = t('.failure')
+    end
+    redirect_to @test_passage
+   end
+
+  private
+
+  def set_test_passage
+    @test_passage = TestPassage.find(params['test_passage_id'])
+  end
+end
